813835edfd9ab494ab4809a8471ca41c35bfcfd8
PHUC\phuchb <phuchb04@gmail.com>
2026-01-28 15:46:56 +0700

    create employee model and init database migration

---
+++ EmployeeManagement/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
