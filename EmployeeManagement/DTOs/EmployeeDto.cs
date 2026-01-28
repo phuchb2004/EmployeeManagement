0817655ceca96a03783c22adcf9e01348d4c72df
PHUC\phuchb <phuchb04@gmail.com>
2026-01-28 16:42:02 +0700

    add employee dto

---
+++ EmployeeManagement/DTOs/EmployeeDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.DTOs
+{
+
+    public record EmployeeDto(
+        int Id,
+        string Name,
+        string Email,
+        string Department,
+        DateTime DateOfBirth
+    );
+
+    public record CreateEmployeeDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
+        public string Name { get; init; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Email incorrect format")]
+        public string Email { get; init; } = string.Empty;
+
+        public string Department { get; init; } = string.Empty;
+
+        public DateTime DateOfBirth { get; init; }
+    }
+
+    public record UpdateEmployeeDto : CreateEmployeeDto
+    {
+
+    }
+
+}
