229ee3ecfb11bfa114871762ce3f1e2146be12db
PHUC\phuchb <phuchb04@gmail.com>
2026-01-28 23:54:39 +0700

    create controller api for employee - getAll, getById, create, update, delete

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EmployeeManagement.DTOs;
+using EmployeeManagement.Services;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagement.Controllers
@@ -7,5 +10,92 @@ namespace EmployeeManagement.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly EmployeeService _service;
+
+        public EmployeeController(EmployeeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetAllEmployee")]
+        public async Task<ActionResult<List<EmployeeDto>>> GetAllEmployee()
+        {
+            var employees = await _service.GetAllEmployee();
+            return Ok(employees);
+        }
+
+        [HttpGet("GetEmployeeById")]
+        public async Task<ActionResult<EmployeeDto>> GetEmployeeById(int id)
+        {
+            var employee = await _service.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound(new {
+                    message = $"Employee with ID {id} is not found"
+                });
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost("CreateEmployee")]
+        public async Task<ActionResult<EmployeeDto>> CreateEmployee([FromBody] CreateEmployeeDto requestDto)
+        {
+            var newEmployee = new Employee
+            {
+                Name = requestDto.Name,
+                Email = requestDto.Email,
+                Department = requestDto.Department,
+                DateOfBirth = requestDto.DateOfBirth
+            };
+
+            var createdEmployee = await _service.CreateEmployee(newEmployee);
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = createdEmployee.Id }, new
+            {
+                message = "Employee Created!",
+                createdEmployee
+            });
+        }
+
+        [HttpPut("UpdateEmployee")]
+        public async Task<ActionResult<EmployeeDto>> UpdateEmployee(int id, [FromBody] UpdateEmployeeDto requestDto)
+        {
+            var existedEmployee = new Employee
+            {
+                Name = requestDto.Name,
+                Email = requestDto.Email,
+                Department = requestDto.Department,
+                DateOfBirth = requestDto.DateOfBirth
+            };
+
+            var updatedEmployee = await _service.UpdateEmployee(id, existedEmployee);
+
+            if (updatedEmployee == null)
+            {
+                return NotFound(new {
+                    message = $"Update fail, employee with ID {id} is not found!"
+                });
+            }
+            return Ok(new {
+                message = $"Employee {updatedEmployee.Name} is updated",
+                updatedEmployee
+            });
+        }
+
+        [HttpDelete("DeleteEmployee")]
+        public async Task<ActionResult<EmployeeDto>> DeleteEmployee(int id)
+        {
+            var employee = await _service.DeleteEmployee(id);
+            if (employee == null)
+            {
+                return NotFound(new {
+                    message = $"Delete fail, employee with ID {id} is not found!"
+                });
+            }
+
+            return Ok(new {
+                message = "Employee deleted!"
+            });
+        }
     }
 }
